1e4cba682361ea6dff69d315e69f722a3a1c1278
Strife <kevin.lamblin@neuf.fr>
2019-12-12 12:11:15 +0100

    Ajout Herbe sauvage

--- IUT-Valley/src/Metier/Objet/Arbres/Sapin.java
+++ IUT-Valley/src/Metier/Objet/Arbres/Sapin.java
@@ -3,7 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Metier.Objet;
+package Metier.Objet.Arbres;
+
+import Metier.Objet.Arbres.Arbre;
 
 /**
  *
---
+++ IUT-Valley/src/Metier/Objet/HerbeSauvage.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Metier.Objet;
+
+/**
+ *
+ * @author Kevin
+ */
+public class HerbeSauvage extends ObjetNaturel{
+
+    public HerbeSauvage() {
+        super();
+    }
+    
+    @Override
+    public void Interagir() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getType() {
+        return "HerbeSauvage";
+    }
+    
+}
